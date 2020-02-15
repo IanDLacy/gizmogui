78d501077c6d0fb0966a973e73933baf4bfa34f1
Chris <crsayen@gmail.com>
2020-02-14 22:56:22 -0500

    progress

---
+++ src/components/node.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="node">
+    <div>{{ label }}</div>
+    <node 
+      v-for="node in nodes" 
+      :nodes="node.nodes" 
+      :label="node.label"
+      :key="node.id"
+    >
+    </node>
+  </div>
+</template>
+<script>
+  export default { 
+    props: [ 'label', 'nodes' ],
+    name: 'node'
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,74 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Node from './components/node.vue'
 
 Vue.config.productionTip = false
 
+let data = {
+  controller: "MockUSB",
+  relayController:{
+    relays: [
+      {enabled: true},
+      {enabled: false},
+    ]
+  },
+  pwmController: {
+    pwmMonitorUpdateRate: 100,
+    bankA: {
+      frequency: 1000
+    },
+    bankB: {
+      frequency: 1234
+    },
+    pwms: [
+      {
+        enabled: false,
+        dutyCycle: 50
+      },
+      {
+        enabled: false,
+        dutyCycle: 50
+      },
+    ]
+  }
+}
+
+function makenode(key, val, path){
+    let obj = new Object();
+            obj.path = path + '/' + key;
+            obj.label = key;
+            obj.nodes = parsetree(val, obj.path);
+            return obj
+}
+
+function parsetree(item, path) {
+    if (typeof item === "object" && item !== null){
+        let nodes = [];
+        for (let [key, val] of Object.entries(item)){
+            nodes.push(makenode(key, val, path));
+        }
+        return nodes
+    }
+    else if (Array.isArray(item)) {
+        return item.map((value, index) => makenode(String(index), value))
+    }
+    else {
+        return item;
+    }
+}
+
+let tree = new Object()
+tree.label = data.controller
+tree.path = '/' + data.controller
+tree.nodes = parsetree(data);
+
+console.log(tree);
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    tree
+  },
+  components: {
+    Node
+  }
+})
